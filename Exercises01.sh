4faf4dd307e1a0979066297712b86a8827fe60ec
ankursingh <ankursingh.creative@gmail.com>
2025-07-04 15:19:18 +0200

    shell scripts exercises

--- Exercises01.sh
+++ Exercises01.sh
@@ -1,5 +1,15 @@
 #! bin/bash
 
+#EXERCISE-1
+#Create a Script that does the following:
+
+#-Create  2 .txt files: "1.txt" "2.txt"
+#-Inside 1.txt write the content of your pwd. in the long list format
+#-Ask the user what prefix he wants to give to the file "1.txt"
+#-Change the name of "1.txt" adding to it the prefix choosen by the user
+
+
+
 
 echo "create two file 2.txt and 1.txt"
 
